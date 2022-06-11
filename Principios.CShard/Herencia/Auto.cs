928b16a82eb03e77a5da405fa3c85095ae4ac2bc
FabricioOrtiz <fabricio.ortiz.alfaro@gmail.com>
2022-06-10 18:48:21 -0600

    Initial commit

---
+++ Principios.CShard/Herencia/Auto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principios.CShard.Herencia
+{
+    public abstract class Auto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Precio { get; set; }
+
+        public void Avanzar() 
+        {
+            Console.WriteLine($"El auto {Name} , esta avanzo");
+        }
+
+        public void PrintPrecio()
+        {
+            Console.WriteLine($"El auto {Name} , tiene el precio de ${Precio}");
+        }
+    }
+}
---
+++ Principios.CShard/Poliformismo/Index.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principios.CShard.Poliformismo
+{
+    public class Index
+    {
+        /*
+         El polimorfismo se refiere a la posibilidad de definir múltiples clases con funcionalidad diferente, pero con métodos o propiedades denominados de forma idéntica.
+         */
+    }
+
+    public class Alumno : PersonaBase
+    {
+        public override void Saludar()
+        {
+            Console.WriteLine("Holis");
+        }
+    }
+
+    public abstract class PersonaBase
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public abstract void Saludar();
+    }
+}
